bf06250cbd2823760d9e6c83bdf9ceea63fbd9be
Daniela Caus <danicaus.br@gmail.com>
2021-04-27 18:35:29 -0300

    Post SuperTrunfo game made at ImersãoDev event by Alura

---
+++ main.js
@@ -0,0 +1,409 @@
+const cartaTrunfo1 = {
+    nome:"Homem de Ferro",
+    imagem: "http://minhaseriefavorita.com/wp-content/uploads/2020/02/homem-de-ferro-curiosidades-1-minhaseriefavorita.jpg",
+    atributos: {
+        força:4,
+        velocidade:4,
+        habilidade:3,
+        equipamento:5,
+        inteligencia:6
+    }
+}
+
+const cartaTrunfo2 = {
+    nome:"Capitão América",
+    imagem: "https://observatoriodocinema.uol.com.br/wp-content/uploads/2019/09/cropped-CAptain-America-2.jpg",
+    atributos: {
+        força:4,
+        velocidade:5,
+        habilidade:3,
+        equipamento:3,
+        inteligencia:4
+    }
+}
+
+const cartaTrunfo3 = {
+    nome:"Hulk",
+    imagem: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/thor-ragnarok-hulk-scar-1572267017.jpg",
+    atributos: {
+        força:6,
+        velocidade:2,
+        habilidade:4,
+        equipamento:1,
+        inteligencia:1
+    }
+}
+
+const cartaTrunfo4 = {
+    nome:"Feiticeira Escarlate",
+    imagem: "https://ibcdn.canaltech.com.br/stinZ7MQDGSZTj4yApwiYDx_tH4=/512x288/smart/i367281.png",
+    atributos: {
+        força:3,
+        velocidade:3,
+        habilidade:6,
+        equipamento:1,
+        inteligencia:3
+    }
+}
+
+const cartaTrunfo5 = {
+    nome:"Thor",
+    imagem: "https://qph.fs.quoracdn.net/main-qimg-876adc337c4104c5f2930af3f152a5b4",
+    atributos: {
+        força:5,
+        velocidade:5,
+        habilidade:3,
+        equipamento:3,
+        inteligencia:2
+    }
+}
+
+const cartaTrunfo6 = {
+    nome:"Viúva Negra",
+    imagem:"https://rollingstone.uol.com.br/media/_versions/viuva_negra_-_divulgacao_marvel_widelg.jpg",
+    atributos: {
+        força:3,
+        velocidade:4,
+        habilidade:4,
+        equipamento:2,
+        inteligencia:4
+    }
+}
+
+const cartaTrunfo7 = {
+    nome:"Capitã Marvel",
+    imagem:"https://4.bp.blogspot.com/-WE7gwHlSUfk/XIUZiN9wZtI/AAAAAAABBsA/ngx6ftSo0KwxNmaf5-mc6F40GHjT2M8nQCLcBGAs/s1600/captain-marvel-costume-brie-larson-mcu-movie.jpg",
+    atributos: {
+        força:5,
+        velocidade:6,
+        habilidade:6,
+        equipamento:2,
+        inteligencia:3
+    }
+}
+
+const cartaTrunfo8 = {
+    nome:"Pantera Negra",
+    imagem: "https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/08/pantera-negra.jpg",
+    atributos: {
+        força:4,
+        velocidade:5,
+        habilidade:3,
+        equipamento:5,
+        inteligencia:3
+    }
+}
+
+const cartaTrunfo9 = {
+    nome:"Visão",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm2x7TdKHcALNqGXq7lYPdwbRczc3jFHyFqQ&usqp=CAU",
+    atributos: {
+        força:5,
+        velocidade:5,
+        habilidade:6,
+        equipamento:5,
+        inteligencia:6
+    }
+}
+
+const cartaTrunfo10 = {
+    nome:"Doutor Estranho",
+    imagem: "https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q80/wp-content/uploads/2021/02/legiao_Pa2ydfkZjz6i.jpg.jpeg",
+    atributos: {
+        força:3,
+        velocidade:4,
+        habilidade:5,
+        equipamento:2,
+        inteligencia:5
+    }
+}
+
+const cartaTrunfo11 = {
+    nome:"Gavião Arqueiro",
+    imagem: "https://s.aficionados.com.br/imagens/gaviao-arqueiro-1.jpg",
+    atributos: {
+        força:2,
+        velocidade:3,
+        habilidade:3,
+        equipamento:4,
+        inteligencia:3
+    }
+}
+
+const cartaTrunfo12 = {
+    nome:"Soldado Invernal",
+    imagem: "https://itsastampede615821596.files.wordpress.com/2019/04/endgame-2.jpg",
+    atributos: {
+        força:4,
+        velocidade:5,
+        habilidade:3,
+        equipamento:4,
+        inteligencia:3
+    }
+}
+
+const cartaTrunfo13 = {
+    nome:"Homem Formiga",
+    imagem: "http://static.comicvine.com/uploads/original/11116/111163466/4714615-3658903674-ant-m.jpg",
+    atributos: {
+        força:1,
+        velocidade:3,
+        habilidade:3,
+        equipamento:4,
+        inteligencia:3
+    }
+}
+
+const cartaTrunfo14 = {
+    nome:"Homem Aranha",
+    imagem: "https://t2.tudocdn.net/492362?w=646&h=284",
+    atributos: {
+        força:1,
+        velocidade:4,
+        habilidade:2,
+        equipamento:2,
+        inteligencia:2
+    }
+}
+
+const cartaTrunfo15 = {
+    nome:"Falcão",
+    imagem: "https://oretalho.com.br/wp-content/uploads/2021/03/2021-03-22-foto-s%C3%A9ries-falcao-soldado-invernal.jpg",
+    atributos: {
+        força:1,
+        velocidade:5,
+        habilidade:3,
+        equipamento:2,
+        inteligencia:2
+    }
+}
+
+const cartaTrunfo16 = {
+    nome:"Loki",
+    imagem: "https://screenhubweb.files.wordpress.com/2018/08/loki-the-avengers-screenhub-entertainment.jpg?w=740&h=511&crop=1",
+    atributos: {
+        força:2,
+        velocidade:3,
+        habilidade:4,
+        equipamento:3,
+        inteligencia:4
+    }
+}
+
+const cartaTrunfo17 = {
+    nome:"Groot",
+    imagem: "http://pm1.narvii.com/6446/84ade8a0e3b8195d8e977cbe7b1f5ac936ffb99d_00.jpg",
+    atributos: {
+        força:4,
+        velocidade:2,
+        habilidade:5,
+        equipamento:3,
+        inteligencia:1
+    }
+}
+
+const cartaTrunfo18 = {
+    nome:"Senhor das Estrelas",
+    imagem: "https://cinepop.com.br/wp-content/uploads/2018/04/guardi%C3%B5es3.jpg",
+    atributos: {
+        força:2,
+        velocidade:2,
+        habilidade:2,
+        equipamento:2,
+        inteligencia:2
+    }
+}
+
+const cartaTrunfo19 = {
+    nome:"Gamora",
+    imagem: "https://img1.looper.com/img/gallery/what-gamora-almost-looked-like-in-the-mcu/intro-1543503745.jpg",
+    atributos: {
+        força:2,
+        velocidade:2,
+        habilidade:3,
+        equipamento:1,
+        inteligencia:3
+    }
+}
+
+const cartaTrunfo20 = {
+    nome:"Rocket",
+    imagem: "https://static0.srcdn.com/wordpress/wp-content/uploads/2020/04/1-17-e1587735503744.jpg",
+    atributos: {
+        força:3,
+        velocidade:3,
+        habilidade:4,
+        equipamento:3,
+        inteligencia:2
+    }
+}
+
+var cartas = [cartaTrunfo1,cartaTrunfo2,cartaTrunfo3,cartaTrunfo4,cartaTrunfo5,cartaTrunfo6,cartaTrunfo7,cartaTrunfo8,cartaTrunfo9,cartaTrunfo10,cartaTrunfo11,cartaTrunfo12,cartaTrunfo13,cartaTrunfo14,cartaTrunfo15,cartaTrunfo16,cartaTrunfo17,cartaTrunfo18,cartaTrunfo19,cartaTrunfo20]
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizarPlacar()
+atualizaQuantidadeCartas()
+
+function sortearCarta(){
+    var numeroCartaMaquina = Math.floor(Math.random() * (cartas.length))
+    cartaMaquina = cartas[numeroCartaMaquina]
+    
+    cartas.splice(numeroCartaMaquina,1)
+
+    var numeroCartaJogador = Math.floor(Math.random() * (cartas.length))
+    cartaJogador = cartas[numeroCartaJogador]
+
+    cartas.splice(numeroCartaJogador,1)
+    
+    document.querySelector("#btnSortear").disabled = true
+    document.querySelector("#btnJogar").disabled = false
+    exibeCartaJogador()
+    atualizaQuantidadeCartas()
+}
+
+function exibeCartaJogador(){
+    var divCartaJogador = document.querySelector("#carta-jogador")
+    divCartaJogador.style.backgroundImage=`url(${cartaJogador.imagem})`
+    var nome = `<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png"
+    style=" width: inherit; height: inherit; position: absolute;"><p class="carta-subtitle">${cartaJogador.nome}</p>`
+    
+    var opcoesTexto = ""
+    for (var atributo in cartaJogador.atributos){
+        opcoesTexto += `<input type='radio' name="atributo" value="${atributo}">${atributo}: ${cartaJogador.atributos[atributo]}<br>`
+    }
+    
+    html = `<div class="carta-status">${opcoesTexto}</div>`
+    divCartaJogador.insertAdjacentHTML("afterbegin",nome)
+    divCartaJogador.insertAdjacentHTML("beforeend",html)
+
+    var instrucao = document.querySelector("h2")
+    instrucao.innerHTML = "Selecione um dos atributos em sua carta"
+}
+
+function exibeCartaMaquina(){
+    var divCartaMaquina = document.querySelector("#carta-maquina")
+    divCartaMaquina.style.backgroundImage=`url(${cartaMaquina.imagem})`
+    var nome = `<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png"
+    style=" width: inherit; height: inherit; position: absolute;"><p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    
+    var opcoesTexto = ""
+    for (var atributo in cartaJogador.atributos){
+        opcoesTexto += `<p type="text" name="atributo" value="${atributo}">${atributo}: ${cartaMaquina.atributos[atributo]}</p>`
+    }
+    
+    html = `<div class="carta-status">${opcoesTexto}</div>`
+    divCartaMaquina.insertAdjacentHTML("afterbegin",nome)
+    divCartaMaquina.insertAdjacentHTML("beforeend",html)
+}
+
+function obterAtributoSelecionado (){
+    var radioAtributo = document.getElementsByName("atributo")
+    for (var i=0; i<radioAtributo.length; i++){
+        if(radioAtributo[i].checked){
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar(){
+    var atributoSelecionado = obterAtributoSelecionado()
+    campoResultado = document.querySelector("#resultado-final")
+    if (atributoSelecionado == undefined){
+        alert("Selecione um dos atributos!")
+    } else if(cartaMaquina.atributos[atributoSelecionado] > cartaJogador.atributos[atributoSelecionado]){
+        campoResultado.innerHTML = `<p class="resultado-final">Você Perdeu!</p>`
+        pontosMaquina++
+        exibeCartaMaquina()
+        document.querySelector("#btnJogar").disabled = true
+        document.querySelector("#btnProximaRodada").disabled = false
+    } else if (cartaMaquina.atributos[atributoSelecionado] == cartaJogador.atributos[atributoSelecionado]){
+        campoResultado.innerHTML = `<p class="resultado-final">Empate!</p>`
+        exibeCartaMaquina()
+        document.querySelector("#btnJogar").disabled = true
+        document.querySelector("#btnProximaRodada").disabled = false
+    } else if (cartaMaquina.atributos[atributoSelecionado] < cartaJogador.atributos[atributoSelecionado]){
+        campoResultado.innerHTML = `<p class="resultado-final">Você ganhou!</p>`
+        pontosJogador++
+        exibeCartaMaquina()
+        document.querySelector("#btnJogar").disabled = true
+        document.querySelector("#btnProximaRodada").disabled = false
+    } 
+    atualizarPlacar()
+}
+
+function atualizarPlacar(){
+    var divPlacar = document.querySelector("#placar")
+    var html = `Jogador ${pontosJogador} | ${pontosMaquina} Máquina `
+    divPlacar.innerHTML = html
+}
+
+function atualizaQuantidadeCartas(){
+    var divQuantidadeCartas = document.querySelector("#quantidade-cartas")
+    var html = `Quantidade de cartas no jogo: ${cartas.length}`
+    divQuantidadeCartas.innerHTML = html
+}
+
+function proximaRodada(){
+    var divCartas = document.querySelector("#cartas")
+    var divCartaJogador = document.querySelector("#carta-jogador")
+    var divCartaMaquina = document.querySelector("#carta-maquina")
+    divCartaJogador.style.backgroundImage=``
+    divCartaMaquina.style.backgroundImage=``
+    divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div><div id="resultado-final" class="carta"></div><div id="carta-maquina" class="carta"></div>`
+
+    var campoResultado = document.querySelector("#resultado-final")
+    campoResultado.innerHTML = ``
+
+    var instrucoes = document.querySelector("h2")
+    instrucoes.innerHTML = ``
+
+    document.querySelector("#btnSortear").disabled = false
+    document.querySelector("#btnProximaRodada").disabled = true
+
+    if(cartas.length == 0){
+        alert("Acabou o jogo!")
+        document.querySelector("#btnSortear").disabled = true
+        var telaFimDeJogo = document.querySelector("#reiniciar-jogo") 
+        telaFimDeJogo.innerHTML = `<button type="button" id="btnReiniciarJogo" onclick="reiniciarJogo()">Reiniciar Jogo</button>`
+        telaFimDeJogo = document.querySelector("#divBotaoSortear")
+        telaFimDeJogo.innerHTML = ``
+        telaFimDeJogo = document.querySelector("#divBotaoProximaRodada")
+        telaFimDeJogo.innerHTML = ``
+        if (pontosJogador > pontosMaquina){
+            campoResultado.innerHTML = `<p class="resultado-final">Parabéns, você Venceu!</p>`
+        } else if (pontosJogador < pontosMaquina){
+            campoResultado.innerHTML = `<p class="resultado-final">Você Perdeu! :(</p>`
+        } else if (pontosJogador == pontosMaquina){
+            campoResultado.innerHTML = `<p class="resultado-final">Empate!</p>`
+        }
+        
+    }
+
+}
+
+function reiniciarJogo(){
+    pontosJogador = 0
+    pontosMaquina = 0
+    cartas = [cartaTrunfo1,cartaTrunfo2,cartaTrunfo3,cartaTrunfo4,cartaTrunfo5,cartaTrunfo6,cartaTrunfo7,cartaTrunfo8,cartaTrunfo9,cartaTrunfo10,cartaTrunfo11,cartaTrunfo12,cartaTrunfo13,cartaTrunfo14,cartaTrunfo15,cartaTrunfo16,cartaTrunfo17,cartaTrunfo18,cartaTrunfo19,cartaTrunfo20]
+    atualizarPlacar()
+    atualizaQuantidadeCartas()
+    var campoResultado = document.querySelector("#resultado-final")
+    campoResultado.innerHTML = ``
+    var telaInicioDeJogo = document.querySelector("#reiniciar-jogo") 
+    telaInicioDeJogo.innerHTML = `<div id="cartas">
+                                        <div id="carta-jogador" class="carta">
+                                            <img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png"
+                                            style=" width: inherit; height: inherit; position: absolute;">]
+                                        </div>
+                                        <div id="carta-maquina" class="carta">
+                                            <img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png"
+                                            style=" width: inherit; height: inherit; position: absolute;">
+                                        </div>
+                                  </div>
+                                  <button type="button" id="btnJogar" onclick="jogar()" disabled="true">Jogar</button>
+                                  <button type="button" id="btnProximaRodada" onclick="proximaRodada()" disabled="true">Proxima rodada</button>`
+    telaInicioDeJogo = document.querySelector("#divBotaoSortear")
+    telaInicioDeJogo.innerHTML = `<button type="button" id="btnSortear" onclick="sortearCarta()">Sortear carta</button>`                            
+    document.querySelector("#btnSortear").disabled = false
+}
